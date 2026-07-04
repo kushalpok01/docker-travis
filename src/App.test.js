fb3c327828e56712490d49f383246e7299c926c4
Kushal <kushalpok01@gmail.com>
2026-07-04 12:44:33 +0545

    fix app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import { createRoot } from 'react-dom/client';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,8 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
- it('renders the App component without crashing', () => {
- const div = document.createElement('div');
- ReactDOM.render(<App />, div);
- ReactDOM.unmountComponentAtNode(div);
+it('renders the App component without crashing', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
